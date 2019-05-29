c8a0a14859be9d05998eb1cc105f4131b2507896
Mike Hageali <45021989+zmanphx@users.noreply.github.com>
2019-05-29 12:18:42 -0700

    fix format

--- models/emailSent.js
+++ models/emailSent.js
@@ -1,26 +1,19 @@
 module.exports = function(sequelize, DataTypes) {
-    var emailSent = sequelize.define("emailSent", {
-      subject: DataTypes.STRING,
-      recipient : DataTypes.STRING,
-      from:DataTypes.STRING,
-      body:DataTypes.STRING,
-      sentdt: DataTypes.DATE
-      
-     
+  var emailSent = sequelize.define("emailSent", {
+    subject: DataTypes.STRING,
+    recipient: DataTypes.STRING,
+    from: DataTypes.STRING,
+    body: DataTypes.STRING,
+    sentdt: DataTypes.DATE
+  });
+
+  emailSent.associate = function(models) {
+    emailSent.belongsTo(models.hiker, {
+      foreignKey: {
+        allowNull: false
+      }
     });
- 
-    emailSent.associate = function(models) {
-        
-        emailSent.belongsTo(models.hiker, {
-          foreignKey: {
-            allowNull: false
-          }
-        });
-      };
-    
- 
- 
- 
-    return emailSent;
   };
-  
+
+  return emailSent;
+};
