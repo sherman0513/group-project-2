43c2ebd367ab75efd6e6950a97ee6e6b09029951
Amy Merrill <amous.fullstack@gmail.com>
2019-06-03 01:15:14 -0700

    mike and amy slaying it

--- models/hiker.js
+++ models/hiker.js
@@ -1,15 +1,15 @@
 /* eslint-disable linebreak-style */
 module.exports = function(sequelize, DataTypes) {
-  var hiker = sequelize.define("hiker", {
-    fname: {
+  var Hiker = sequelize.define("Hiker", {
+    fName: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       validate: { len: [1] }
     },
 
-    lname: {
+    lName: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       validate: { len: [1] }
     },
 
@@ -19,34 +19,35 @@ module.exports = function(sequelize, DataTypes) {
     state: DataTypes.STRING,
     zip: {
       type: DataTypes.STRING,
-      allowNull: false,
+      allowNull: true,
       validate: { len: [5] }
     },
     email: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: { len: [5] }
-    },
+    }
     // c1: DataTypes.BOOLEAN,
     // c2: DataTypes.BOOLEAN,
     // c3: DataTypes.BOOLEAN,
     // c4: DataTypes.BOOLEAN,
     // c5: DataTypes.BOOLEAN,
-    longit: DataTypes.DOUBLE,
-    latit: DataTypes.DOUBLE
+
+    // longit: DataTypes.DOUBLE,
+    // latit: DataTypes.DOUBLE
   });
 
-  hiker.associate = function(models) {
-    hiker.hasMany(models.searchFav, {
-      onDelete: "cascade"
-    });
-  };
+  // Hiker.associate = function(models) {
+  //   Hiker.hasMany(models.SearchFav, {
+  //     onDelete: "cascade"
+  //   });
+  // };
 
-  hiker.associate = function(models) {
-    hiker.hasMany(models.emailSent, {
-      onDelete: "cascade"
-    });
-  };
+  // Hiker.associate = function(models) {
+  //   Hiker.hasMany(models.EmailSent, {
+  //     onDelete: "cascade"
+  //   });
+  // };
 
-  return hiker;
+  return Hiker;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,50 +1,53 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
+  // Get all users
+  app.get("/api/users", function(req, res) {
+    db.Hiker.findAll({}).then(function(dbHikers) {
+      res.json(dbHikers);
+    });
+  });
+
+  // app.post("/api/authors", function(req, res) {
+  //   // Create an Author with the data available to us in req.body
+  //   console.log(req.body);
+  //   db.Author.create(req.body).then(function(dbAuthor) {
+  //     res.json(dbAuthor);
+  //   });
+  // });
+
+  // app.get("/api/examples", function(req, res) {
+  //   db.Example.findAll({}).then(function(dbExamples) {
+  //     res.json(dbExamples);
+  //   });
+  // });
+
+  // Create a new user
+  app.post("/api/users", function(req, res) {
+    console.log("i'm in post");
+    db.Hiker.create(req.body).then(function(dbHiker) {
+      res.json(dbHiker);
+    });
+  });
+
+  // Delete a user by id
+  app.delete("/api/users/:id", function(req, res) {
+    db.Hiker.destroy({ where: { id: req.params.id } }).then(function(dbHiker) {
+      res.json(dbHiker);
+    });
+  });
+
+  //Get all favorite hikes
+  app.get("/api/favehikes", function(req, res) {
     db.Example.findAll({}).then(function(dbExamples) {
       res.json(dbExamples);
     });
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
+  // Create a new favorite
+  app.post("/api/favehikes", function(req, res) {
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
   });
-
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(
-      dbExample
-    ) {
-      res.json(dbExample);
-    });
-  });
 };
-
-// var express = require("express");
-
-// var router = express.Router();
-
-// Import the model (burger.js) to use its database functions.
-// var trails = require("../models/burger.js");
-
-// Create all our routes and set up logic within those routes where required.
-// router.get("/api/trails", function(req, res) {
-//   $.ajax({
-//     url: queryURL,
-//     method: "GET"
-//   }).then(function(response) {
-
-//   trails.all(function(data) {
-//     var hbsObject = {
-//       trails: data
-//     };
-//     console.log(hbsObject);
-//     console.log("Inside get all");
-//     res.render("trails", hbsObject);
-//   });
-// });
