11875ef2a20c32871c0f47327e0fc7efbb25f07a
Amy Merrill <amous.fullstack@gmail.com>
2019-06-06 02:41:50 -0700

    implemented mike's code

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,130 +1,179 @@
-console.log("LINKED");
-// Get references to page elements
-// var $saveFavTrail = $("#favorite-hike");
-// var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submitSignUp");
-// var $searchLocation = $("#locationInput");
+/* eslint-disable indent */
+$(document).ready(function() {
+  console.log("LINKED");
 
-var $fName = $("#fName");
-var $lName = $("#lName");
-var $email = $("#inputEmail");
-// var $password = $("#inputPassword");
-var $address = $("#inputAddress");
-var $city = $("#inputCity");
-var $state = $("#inputState");
-var $zip = $("#inputZip");
-
-// The API object contains methods for each kind of request we'll make
-var API = {
-  saveFavTrail: function(example) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/trails",
-      // url: "api/examples",
-      data: JSON.stringify(example)
+  $(document).on("click", ".btn-info", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    //building the save trail object
+    var newTrailSav = {
+      trailName: $(this).data("trailName"),
+      type: $(this).data("type"),
+      summary: $(this).data("summary"),
+      difficulty: $(this).data("difficulty"),
+      stars: $(this).data("stars"),
+      location: $(this).data("location"),
+      url: $(this).data("url"),
+      image: $(this).data("image"),
+      length: $(this).data("length"),
+      longit: $(this).data("longit"),
+      latit: $(this).data("latit"),
+      conditionStat: $(this).data("conditionStat"),
+      conditionDetails: $(this).data("condDetails"),
+      conditionDate: $(this).data("condDate"),
+      trailid: $(this).data("myval"),
+      HikerId: $(this).data("hikerid")
+    };
+  });
+  var $submitBtn = $("#submitSignUp");
+  var $exampleList = $("#example-list");
+  // var $searchLocation = $("#locationInput");
+  var $fName = $("#fName");
+  var $lName = $("#lName");
+  var $email = $("#inputEmail");
+  // var $password = $("#inputPassword");
+  var $address = $("#inputAddress");
+  var $city = $("#inputCity");
+  var $state = $("#inputState");
+  var $zip = $("#inputZip");
+  // The API object contains methods for each kind of request we'll make
+  var API = {
+    saveFavTrail: function(newTrailSav) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/trails",
+        // url: "api/examples",
+        // data: JSON.stringify(example)
+        data: newTrailSav
+      });
+    },
+    getFavTrails: function(loc) {
+      console.log("GetFavTrails");
+      console.log("passLoc", loc);
+      return $.ajax({
+        url: "/trails/" + loc,
+        type: "GET"
+      });
+    },
+    // getExamples: function() {
+    //   return $.ajax({
+    //     url: "api/examples",
+    //     type: "GET"
+    //   });
+    deleteExample: function(id) {
+      return $.ajax({
+        url: "api/examples/" + id,
+        type: "DELETE"
+      });
+    },
+    saveUser: function(user) {
+      return $.ajax({
+        headers: {
+          "Content-Type": "application/json"
+        },
+        type: "POST",
+        url: "api/users",
+        data: JSON.stringify(user)
+      });
+    }
+  };
+  // refreshExamples gets new examples from the db and repopulates the list
+  var refreshFavTrails = function() {
+    API.getFavTrails().then(function(data) {
+      var $favTrails = data.map(function(favTrail) {
+        var $a = $("<a>")
+          .text(favTrail.text)
+          .attr("href", "/favtrail/" + favTrail.id);
+        var $li = $("<li>")
+          .attr({
+            class: "list-group-item",
+            "data-id": favTrail.id
+          })
+          .append($a);
+        var $button = $("<button>")
+          .addClass("btn btn-danger float-right delete")
+          .text("Favorite");
+        $li.append($button);
+        return $li;
+      });
+      $exampleList.empty();
+      $exampleList.append($favTrails);
     });
-  },
-  getFavTrails: function() {
-    return $.ajax({
-      url: "api/trails",
-      type: "GET"
+  };
+  // handleFormSubmit is called whenever we submit a new example
+  // Save the new example to the db and refresh the list
+  var handleFormSubmit = function(event) {
+    event.preventDefault();
+    console.log("i'm here");
+    var user = {
+      fName: $fName.val().trim(),
+      lName: $lName.val().trim(),
+      email: $email.val().trim(),
+      // password: $password.val().trim(),
+      address: $address.val().trim(),
+      city: $city.val().trim(),
+      state: $state.val().trim(),
+      zip: $zip.val().trim()
+    };
+    console.log(user);
+    if (!(user.fName && user.lName)) {
+      alert("You must enter a first and last name!");
+      return;
+    }
+    console.log("I'm on line 143");
+    API.saveUser(user).then(function() {
+      console.log("callback");
+      // refreshForm();
     });
-  },
-
-  deleteExample: function(id) {
-    return $.ajax({
-      url: "api/examples/" + id,
-      type: "DELETE"
-    });
-  },
-  saveUser: function(user) {
-    return $.ajax({
-      headers: {
-        "Content-Type": "application/json"
-      },
-      type: "POST",
-      url: "api/users",
-      data: JSON.stringify(user)
-    });
-  }
-};
-
-// refreshExamples gets new examples from the db and repopulates the list
-// var refreshFavTrails = function() {
-//   API.getFavTrails().then(function(data) {
-//     var $favTrails = data.map(function(favTrail) {
-//       var $a = $("<a>")
-//         .text(favTrail.text)
-//         .attr("href", "/favtrail/" + favTrail.id);
-
-//       var $li = $("<li>")
-//         .attr({
-//           class: "list-group-item",
-//           "data-id": favTrail.id
-//         })
-//         .append($a);
-
-//       var $button = $("<button>")
-//         .addClass("btn btn-danger float-right delete")
-//         .text("Favorite");
-
-//       $li.append($button);
-
-//       return $li;
-//     });
-
-//     $exampleList.empty();
-//     $exampleList.append($favTrails);
-//   });
-// };
-
-
-// handleFormSubmit is called whenever we submit a new example
-// Save the new example to the db and refresh the list
-var handleFormSubmit = function(event) {
-  event.preventDefault();
-  console.log("i'm here");
-
-  var user = {
-    fName: $fName.val().trim(),
-    lName: $lName.val().trim(),
-    email: $email.val().trim(),
-    // password: $password.val().trim(),
-    address: $address.val().trim(),
-    city: $city.val().trim(),
-    state: $state.val().trim(),
-    zip: $zip.val().trim()
+    $fName.val("");
+    $lName.val("");
+    $email.val("");
+    $password.val("");
+    $address.val("");
+    $city.val("");
+    $state.val("");
+    $zip.val("");
   };
 
-  console.log(user);
-  if (!(user.fName && user.lName)) {
-    alert("You must enter a first and last name!");
-    return;
-  }
-  console.log("I'm on line 143");
+  $("#searchLocation2").on("click", function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    console.log("click me");
+    varloc = $("#locations")
+      .val()
+      .trim();
 
-  API.saveUser(user).then(function() {
-    console.log("callback");
-    // refreshForm();
+    if (varloc.length === 0) {
+      alert("Please enter a location!");
+      return;
+    }
+    $(".bootstrap-tagsinput input").keydown(function(event) {
+      if (event.which == 13) {
+        $(this).blur();
+        $(this).focus();
+        return false;
+      }
+    });
+    window.location = "/trails/" + varloc;
+    // API.getFavTrails(varloc)
+    // .done(res => {
+    //     console.log('res',res);
+    // })
+    // .fail()
   });
-};
-
-// handleDeleteBtnClick is called when an example's delete button is clicked
-// Remove the example from the db and refresh the list
-// var handleDeleteBtnClick = function() {
-//   var idToDelete = $(this)
-//     .parent()
-//     .attr("data-id");
-
-//   API.deleteExample(idToDelete).then(function() {
-//     refreshFavTrails();
-//   });
-// };
-
-// Add event listeners to the submit and delete buttons
-$submitBtn.on("click", handleFormSubmit);
-// $exampleList.on("click", ".delete", handleDeleteBtnClick);
-// $saveFavTrail.on("click", saveFavTrail);
+  var handleDeleteBtnClick = function() {
+    var idToDelete = $(this)
+      .parent()
+      .attr("data-id");
+    API.deleteExample(idToDelete).then(function() {
+      refreshFavTrails();
+    });
+  };
+  // Add event listeners to the submit and delete buttons
+  $submitBtn.on("click", handleFormSubmit);
+  $exampleList.on("click", ".delete", handleDeleteBtnClick);
+  // $saveFavTrail.on("click", saveFavTrail);
+});
